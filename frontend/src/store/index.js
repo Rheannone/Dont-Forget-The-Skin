17444df6bf6c61a78d1299619d95848a8618d432
Rheannone <rheannone@gmail.com>
2021-02-18 16:57:30 -0500

    frontend auth complete

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { restoreCSRF, csrfFetch } from './csrf';
 
 const rootReducer = combineReducers({
   // add reducer functions here
@@ -19,5 +20,14 @@ if (process.env.NODE_ENV === "production") {
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 export default configureStore;
