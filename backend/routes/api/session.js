af46a651da8589d25a258a58d3a5a99016e2f2ab
Rheannone <rheannone@gmail.com>
2021-02-18 12:43:03 -0500

    get user routes complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -27,5 +27,29 @@ router.post(
     }),
   );
 
+  router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+  // Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+  
+  
+
 
 module.exports = router;
