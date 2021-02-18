4fa640c33cf370dd740032f08dc797232fdfb831
Rheannone <rheannone@gmail.com>
2021-02-18 17:25:49 -0500

    session store happening

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { restoreCSRF, csrfFetch } from '../src/store/csrf';
 import configureStore from './store';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
