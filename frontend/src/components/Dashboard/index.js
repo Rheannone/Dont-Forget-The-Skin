0964078722e2a2e0a20b18a8c8c096c4d194858b
Rheannone <rheannone@gmail.com>
2021-02-28 19:55:16 -0500

    submit for is styled

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -12,7 +12,6 @@ function Dashboard() {
   const dispatch = useDispatch();
   const history = useHistory();
   const { menu, setMenu } = useMenu()
-  // const {listRefresh, setListRefresh} = useMenu()
   const sessionUser = useSelector(state => state.session.user);
   const taskItems = useSelector(state => state.dashboard.list)
 
@@ -20,7 +19,6 @@ function Dashboard() {
 
     const handleDelete = (e) => {
     dispatch(destroyTask(e.target.value))
-    // dispatch(getList(sessionUser.id)) 
   }
 
 
@@ -44,6 +42,14 @@ const length = taskItems?.length
     left: (menu === 'open' ? "500px" : "0"),
   };
 
+  let getDay = () => {
+    let d = new Date()
+    let n = d.getDay()
+    return n;
+  }
+
+  let item;
+
   return (
     <>
 
@@ -51,28 +57,344 @@ const length = taskItems?.length
     <div className="dashboard-container"
     style={dashboardContainer}>
     <SliderMenu />
+    <div className="dash-greeting">
+    Hello, {sessionUser.username.toUpperCase()}
+    </div>
     <div className="routine-container">
-    Hello, {sessionUser.username}
-    <h1>List Items</h1>
-
-    {(taskItems?.length === 0 ? <p>start your skincare journey. click the '+' to add your first step</p> :<ul>
+   
+    {(getDay() === 0) ? <div><div><h1>Today's Routine</h1></div>{(taskItems?.length === 0 ? <p>start your skincare journey. click the '+' to add your first step</p> :<ul>
+    <h4>Morning</h4>
       {taskItems?.map(item =>(
-        <li key={item.id}><p>{item.singleStep}</p><p>{item.tags}</p><button value={item.id} onClick={handleDelete}>x</button><button>edit</button>
-        {(item.lengthInMin > 0) ? <h4>{item.lengthInMin} min</h4> : null}</li>
+        <li key={item.id} className="product-list">{(item.sun === true && item.morning === true) ?  
+        <div>
+          <div>
+          {(item.morning === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
         
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
         
-      ) )}
+        : null}
+        </li>
+                              ) 
+                      )
+      }
       
-    </ul> )}
+    </ul> 
+    )
+  } 
+  <ul>
+  <h4>Evening</h4>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.sun === true && item.night === true) ?  
+        <div>
+          <div>
+
+          {(item.night === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul>
+  
+  
+  
+   </div> : null}
+
+   {(getDay() === 1) ? <div><div><h1>Monday's Routine</h1></div>{(taskItems?.length === 0 ? <p>start your skincare journey. click the '+' to add your first step</p> :<ul>
+    <h4>Morning</h4>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.mon === true && item.morning === true) ?  
+        <div>
+          <div>
+          
+          {(item.morning === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul> 
+    )
+  } 
+  <ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.mon === true && item.night === true) ?  
+        <div>
+          <div>
+          <h4>Evening</h4>
+          {(item.night === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul>
+  
+  
+  
+   </div> : null}
+
+   {(getDay() === 2) ? <div><div><h1>Tuesday's Routine</h1></div>{(taskItems?.length === 0 ? <p>start your skincare journey. click the '+' to add your first step</p> :<ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.tues === true && item.morning === true) ?  
+        <div>
+          <div>
+          <h4>Morning</h4>
+          {(item.morning === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul> 
+    )
+  } 
+  <ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.tues === true && item.night === true) ?  
+        <div>
+          <div>
+          <h4>Evening</h4>
+          {(item.night === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul>
+  
+  
+  
+   </div> : null}
+   {(getDay() === 3) ? <div><div><h1>Wednesday's Routine</h1></div>{(taskItems?.length === 0 ? <p>start your skincare journey. click the '+' to add your first step</p> :<ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.wed === true && item.morning === true) ?  
+        <div>
+          <div>
+          <h4>Morning</h4>
+          {(item.morning === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul> 
+    )
+  } 
+  <ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.wed === true && item.night === true) ?  
+        <div>
+          <div>
+          <h4>Evening</h4>
+          {(item.night === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul>
+  
+  
+  
+   </div> : null}
+   {(getDay() === 4) ? <div><div><h1>Thursday's Routine</h1></div>{(taskItems?.length === 0 ? <p>start your skincare journey. click the '+' to add your first step</p> :<ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.thur === true && item.morning === true) ?  
+        <div>
+          <div>
+          <h4>Morning</h4>
+          {(item.morning === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul> 
+    )
+  } 
+  <ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.thur === true && item.night === true) ?  
+        <div>
+          <div>
+          <h4>Evening</h4>
+          {(item.night === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul>
+  
+  
+  
+   </div> : null}
+   {(getDay() === 5) ? <div><div><h1>Friday's Routine</h1></div>{(taskItems?.length === 0 ? <p>start your skincare journey. click the '+' to add your first step</p> :<ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.fri === true && item.morning === true) ?  
+        <div>
+          <div>
+          <h4>Morning</h4>
+          {(item.morning === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul> 
+    )
+  } 
+  <ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.fri === true && item.night === true) ?  
+        <div>
+          <div>
+          <h4>Evening</h4>
+          {(item.night === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul>
+  
+  
+  
+   </div> : null}
+   {(getDay() === 6) ? <div><div><h1>Saturday's Routine</h1></div>{(taskItems?.length === 0 ? <p>start your skincare journey. click the '+' to add your first step</p> :<ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.sat === true && item.morning === true) ?  
+        <div>
+          <div>
+          <h4>Morning</h4>
+          {(item.morning === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul> 
+    )
+  } 
+  <ul>
+      {taskItems?.map(item =>(
+        <li key={item.id} className="product-list">{(item.sat === true && item.night === true) ?  
+        <div>
+          <div>
+          <h4>Evening</h4>
+          {(item.night === true) ? <div><h3>{item.singleStep}</h3>
+          <h5>{item.type}</h5></div>  : null}
+        
+        <button className="delete-btn" value={item.id} onClick={handleDelete}>x</button>
+            </div>
+        </div> 
+        
+        : null}
+        </li>
+                              ) 
+                      )
+      }
+      
+    </ul>
+   </div> : null}
 
     </div>
+    <div className="stats-container">
 
-    <p>Monday Routine</p>
-        
-    
+
+    </div>
     </div>
     </>
   );
+  
 }
 
 export default Dashboard;
